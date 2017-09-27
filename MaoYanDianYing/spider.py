70fbc4721a290df97d99847af38038148e6e3cd5
淼 <447071571@qq.com>
2017-09-27 14:02:34 +0800

    first commit

---
+++ MaoYanDianYing/spider.py
@@ -0,0 +1,51 @@
+import requests
+import json
+from requests.exceptions import RequestException
+import re
+from multiprocessing import Pool
+from pyquery import PyQuery as pq
+
+def get_one_page(url):
+    response = requests.get(url)
+    try:
+        if response.status_code == 200:
+            return response.text
+        return None
+    except RequestException:
+        return None
+
+
+def parse_one_page(html):
+    pattern = re.compile('<dd>.*?board-index.*?>(\d+)</i>.*?data-src="(.*?)".*?name">'
+                         + '<a.*?>(.*?)</a>.*?star">(.*?)</p>.*?releasetime">(.*?)</p>.*?integer">'
+                        + '(.*?)</i>.*?fraction">(.*?)</i>.*?</dd>', re.S)
+    items = re.findall(pattern, html)
+    for item in items:
+        yield {
+            'rank': item[0],
+            'img_src': item[1],
+            'name': item[2],
+            'actors': item[3].strip()[3:],
+            'releasetime': item[4].strip()[5:],
+            'score': item[5] + item[6]
+        }
+
+
+def write_to_file(content):
+    with open('result.txt', 'a', encoding='utf-8') as f:
+        f.write(json.dumps(content, ensure_ascii=False) + '\n')
+
+
+def main(offset):
+    url = 'https://maoyan.com/board/4?offset={}'.format(offset)
+    html = get_one_page(url)
+    content = parse_one_page(html)
+    for item in content:
+        write_to_file(item)
+
+if __name__ == '__main__':
+    # for i in range(10):
+    #     main(i*0)
+    pool = Pool()
+    pool.map(main, [i*0 for i in range(10)])
+
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # webSpider
 Python 网络爬虫
+
+猫眼电影100
+正则表达式
